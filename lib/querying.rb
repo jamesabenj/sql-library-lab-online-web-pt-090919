a6990ee5c7a0c6ec6cd034e1376401df04dde125
James Benjamin <james.abenj@gmail.com>
2019-11-26 02:47:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,10 +26,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
+  "SELECT MAX(series.title)
   FROM series
   INNER JOIN characters
-  ON characters.species = 'human'"
+  WHERE characters.species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
