0cc172dc853bfb82e675941c92085ef09041fc70
James Benjamin <james.abenj@gmail.com>
2019-11-26 03:05:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,5 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(characters.name)
   FROM characters
-  INNER JOIN character_books
-  ON characters.id = character_books.character_id
   ORDER BY characters.name DESC"
 end
